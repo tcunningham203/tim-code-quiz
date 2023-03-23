8b845ae39ed25ccbbee5899d22c41ffef9e6a86b
tcunningham203 <tcunningham203@gmail.com>
2023-03-23 20:41:26 +0900

    added quiz questions

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # tim-code-quiz
 A timed quiz about Javascript fundamentals that stores high scores. 
+
+
+
+## Credits
+I used the mini project from "04-Web-Apis" to help understand the local storage.
+
+I used help from this website to figure out how to do the quiz. However, their quiz was pretty different from what the challenge called for, so I had to change a lot. I basically just kept the way they had all the questions as 1 variable but a lot of other stuff was changed.
+https://www.geeksforgeeks.org/how-to-create-a-simple-javascript-quiz/
---
+++ js/script.js
@@ -0,0 +1,53 @@
+// The questions 
+var quizQuestions = [{
+    id: 0,
+    q: "Commonly used data types do NOT include:",
+    a: [{ text: "strings", isCorrect: false },
+        { text: "booleans", isCorrect: false },
+        { text: "alerts", isCorrect: true },
+        { text: "numbers", isCorrect: false }
+    ]
+
+},
+{
+    id: 1,
+    q: "The condition of an if/else statement is enclosed within ______.",
+    a: [{ text: "quotes", isCorrect: false },
+        { text: "curly brackets", isCorrect: false },
+        { text: "parentheses", isCorrect: true },
+        { text: "square brackets", isCorrect: false }
+    ]
+
+},
+
+{
+    id: 2,
+    q: "Arrays in Javascript can be used to store ______.",
+    a: [{ text: "numbers and strings", isCorrect: false },
+        { text: "other arrays", isCorrect: false },
+        { text: "booleans", isCorrect: false },
+        { text: "all of the above", isCorrect: true }
+    ]
+
+},
+{
+    id: 3,
+    q: "String values must be enclosed within ______ when being assigned to variables.",
+    a: [{ text: "quotes", isCorrect: true },
+        { text: "curly brackets", isCorrect: false },
+        { text: "commas", isCorrect: false },
+        { text: "parenthesis", isCorrect: false }
+    ]
+
+},
+{
+    id: 4,
+    q: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    a: [{ text: "Javascript", isCorrect: false },
+        { text: "terminal/bash", isCorrect: false },
+        { text: "for loops", isCorrect: false },
+        { text: "console.log", isCorrect: true }
+    ]
+
+}
+]
