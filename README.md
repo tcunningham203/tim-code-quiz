7aaed90869b3e1cbf5472dd80bceb1baff664f35
tcunningham203 <tcunningham203@gmail.com>
2023-04-05 19:33:45 +0900

    wrote readme and finished local storage

--- README.md
+++ README.md
@@ -1,6 +1,53 @@
-# tim-code-quiz
-A timed quiz about Javascript fundamentals that stores high scores. 
+# Javascript Quiz
 
+## Description
+
+This Javascript Quiz is a 5 question quiz that asks questions about the programming language Javascript. It was made using HTML, CSS, and Javascript. After the 5 questions have been answered, you can save a high score in local storage. You can find more specifics in the "Usage" section. 
+
+## Table of Contents
+- [Link](#link)
+- [Screenshot](#screenshot)
+- [Usage](#usage)
+- [Development](#development)
+- [Credits](#credits)
+
+## Link
+
+This is a link to the application.
+
+https://tcunningham203.github.io/tim-code-quiz/
+
+## Screenshot
+
+This is what the website looks like when you open it.
+
+![AppScreenshot1](/assets/screenshots/website-screenshot.png?raw=true "Screenshot of Deployed Application- Instruction Screen")
+
+## Usage
+When you load up the site, the instructions for the user are available in the description box.
+
+As the instructions say, you have 70 seconds to answer the questions. You will recieve a 10 second penalty for each incorrect answer. At the end, your remaining time is your score. Because of these instructions, the "timer" in the top right is displayed as "score".
+
+When you press the start button, the quiz timer begins counting down from 70. This timer is always displayed in the top right. 
+
+If you answer a question correctly, it proceeds to the next question, and displays "That's right!" briefly in green text. The green text quickly fades away.
+
+If you answer a question incorrectly, it still proceeds to the the next question, but 10 seconds are deducted from the time (your score) and "That's wrong!" is displayed briefly in red text. The red text quickly fades away.
+
+If you have a score of 1 or higher, the quiz goes to the normal game over screen, which praises you and asks you to submit your initials. You are restricted to 3 characters when submitting. Press the "Enter" button to add the score to the high scores page. After pressing enter, you are automatically sent to the high scores page, which will display the top 5 scores. If a 6th score is submitted, the lowest of the 6 scores is removed. 
+
+If you run out of time, the quiz goes to a special game over screen where you are told you have run out of time. You can press the button to try again, but you are not able to submit a score of 0.
+
+At the top, there is a "High Scores" button. You can access this any time to view locally stored high scores. If you view them in the middle of a quiz, the quiz will be cancelled and you must start from the beginning. On the high scores page, you can "go back" to the first page, and you can "clear scores", erasing all scores in local storage.
+
+The site utilizes media queries to change the size of text and buttons depending on the screen size. The "choice" buttons also change positions as well.
+
+## Development
+In this section, I'll briefly discuss some of the challenges and successes with the project, as well as goals for the future. 
+
+This project took way longer than I expected it too. Each step was like pulling teeth. The only easy part was the CSS, which I enjoy doing. By far the most difficult part was the local storage. I had a lot of trouble getting the high scores to display, and even after that was done, I had trouble with the refreshing and erasing without refreshing the page. But in the end, I was able to figure it out. 
+
+Going forward, if I could redo this project, I would have had each "page" be a seperate function or page, so that I could more easily keep track of which elements were hidden and shown. I would also consider implementing a way to randomize the questions, and adding more questions. 
 
 
 ## Credits
@@ -9,5 +56,6 @@ I used the mini project from "04-Web-Apis" to help understand the timer.
 I used help from this website to figure out how to do the quiz. However, their quiz was pretty different from what the challenge called for, so I had to change a lot. I basically just kept the way they had all the questions as 1 variable but a lot of other stuff was changed.
 https://www.geeksforgeeks.org/how-to-create-a-simple-javascript-quiz/
 
-I used help from this website to figure out the high scores and local storage.
-https://michael-karen.medium.com/how-to-save-high-scores-in-local-storage-7860baca9d68
+I used this youtube video for help with the local storage. 
+https://youtu.be/jfOv18lCMmw
+
--- js/script.js
+++ js/script.js
@@ -8,38 +8,38 @@ var timerElement = document.getElementById("timer");
 // As mentioned in the readme, I used the miniproject for this module as a reference for how to create the timer.
 function startTimer() {
     // Sets timer
-    timer = setInterval(function() {
-      timerCount--;
-      timerElement.textContent = timerCount;
-      if (timerCount >= 0) {
-        // Tests if win condition is met
-        if (id == 5 && timerCount > 0) {
-          // Clears interval and stops timer
-          clearInterval(timer);
-          winGame();
+    timer = setInterval(function () {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (timerCount >= 0) {
+            // Tests if win condition is met
+            if (id == 5 && timerCount > 0) {
+                // Clears interval and stops timer
+                clearInterval(timer);
+            }
         }
-      }
-      // Tests if time has run out
-      if (timerCount === 0) {
-        // Clears interval
-        clearInterval(timer);
-        loseGame();
-        timerCount = 0;
-        timerElement.textContent = timerCount;
-      } 
-      // If the time goes into the negatives
-      if (timerCount < 0) {
-        
-        // Clears interval
-        clearInterval(timer);
-        loseGame();
-        timerCount = 0;
-        timerElement.textContent = timerCount;
-      }
-    }, 1000);
-  }
+        // Tests if time has run out
+        if (timerCount === 0) {
+            // Clears interval
+            clearInterval(timer);
+            loseGame();
+            timerCount = 0;
+            timerElement.textContent = timerCount;
+        }
+        // If the time goes into the negatives
+        if (timerCount < 0) {
 
- 
+            // Clears interval
+            clearInterval(timer);
+            loseGame();
+            timerCount = 0;
+            timerElement.textContent = timerCount;
+        }
+
+    }, 1000);
+}
+
+
 
 
 // Buttons
@@ -50,8 +50,7 @@ var highScoresButton = document.getElementById("high-scores-button");
 var introPage = document.getElementById("welcome");
 var introText = document.getElementById("welcome-text");
 
-//win or lose result
-var resultText = document.getElementById("result-text");
+//lose result
 var loseText = document.getElementById("lose-text");
 
 //result between questions
@@ -67,54 +66,162 @@ var btnB = document.getElementById('btnB');
 var btnC = document.getElementById('btnC');
 var btnD = document.getElementById('btnD');
 
+
+
 // hide/reveal the quiz part when you open the page
 var quizAll = document.getElementById("quiz");
 
+//high scores page
+var highScoresTitle = document.getElementById('high-scores-page');
+var btnBack = document.getElementById('btnBack');
+btnBack.addEventListener("click", homePage);
 
 //event listener for start button
 startButton.addEventListener("click", startQuiz);
 
+// clear high score button
+var btnClear = document.getElementById("btnClear")
+btnClear.addEventListener("click", clearScore);
+function clearScore() {
+
+    localStorage.clear();
+    erase();
+    highScoresPage()
+    
+}
+
 //event listener for high score button
-var highScoresList;
-highScoresButton.addEventListener("click", highScores);
+highScoresButton.addEventListener("click", highScoresPage);
+
+function highScoresPage() {
+    console.log("Pressed High Scores");
+    introPage.style.display = 'none';
+    highScoreField.style.display = "none";
+    noText.style.display = "none"
+    yesText.style.display = "none"
+    quizAll.style.display = "none";
+    loseText.style.display = "none";
+    highScoresTitle.style.display = "block";
+    clearInterval(timer);
+    timerCount = 70;
+    timerElement.textContent = timerCount;
+    render();
+};
+
+
+
+function finishQuiz() {
+
+    quizAll.style.display = "none";
+    loseText.style.display = "none";
+    highScoreField.style.display = "block";
+    console.log("Pressed Finish Quiz");
+};
+
+//high score box stuff
+var highScoreField = document.getElementById('input-box');
+
+
+
+
+var textBox = document.getElementById('text-box');
+textBox.addEventListener("keyup", () => {
+
+    console.log(textBox.value);
+    btnEnter.disabled = !textBox.value;
+
+
+});
+
+var highScores = JSON.parse(localStorage.getItem("high-scores-list")) || [];
+var maxListSize = 5;
+
+// var btnEnter = document.getElementById('btnEnter');
+// btnEnter.addEventListener("click", saveScore);
+
+saveScore = e => {
+    console.log("pressed enter")
+    e.preventDefault();
+    console.log(timerElement.innerText)
+
+    var score = {
+        score: timerElement.innerText,
+        name: textBox.value
+
+    };
+
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score)
+    highScores.splice(5);
+    localStorage.setItem("high-scores-list", JSON.stringify(highScores));
+    console.log(score);
+    highScoresPage()
+}
+var highScoresList = document.getElementById('high-scores-list');
+
+
+
+highScoresList.innerHTML = highScores
+    .map(score => {
+        return `<li class="high-score">${score.name} : ${score.score}</li>`;
+    })
+    .join("");
+
+
+function render() {
+    document.getElementById('high-scores-list').innerHTML = highScores
+        .map(score => {
+            return `<li class="high-score">${score.name} : ${score.score}</li>`;
+        })
+        .join("");
+    localStorage.setItem("high-scores-list", JSON.stringify(highScores));
+}
+function erase() {
+    highScores = [];
+    localStorage.setItem("high-scores-list", JSON.stringify(highScores));
+}
+
 
 var id = 0;
+
+//function to go back to the homepage
+function homePage() {
+    quizAll.style.display = "none";
+    introPage.style.display = 'block';
+    introText.style.display = "block";
+    highScoreField.style.display = "none";
+    loseText.style.display = "none";
+    highScoresTitle.style.display = "none";
+    console.log("Pressed Go Back");
+}
 
 
 //function to start the quiz
 function startQuiz() {
-timerCount = 70;
-quizAll.style.display = "block";
-introPage.style.display = 'none';
-introText.style.display = "none";
-id = 0
-console.log("Pressed Start Quiz"); 
-askQuestion(id);
-startTimer()
-};
-
-function finishQuiz() {
-    
-    introPage.style.display = 'block';
-    quizAll.style.display = "none";
-    resultText.style.display = "block";
+    timerCount = 70;
+    quizAll.style.display = "block";
+    introPage.style.display = 'none';
+    introText.style.display = "none";
+    highScoreField.style.display = "none";
     loseText.style.display = "none";
-    console.log("Pressed Finish Quiz"); 
-    };
-
-    
-function highScores() {
-console.log("Pressed High Scores"); 
+    id = 0
+    console.log("Pressed Start Quiz");
+    askQuestion(id);
+    startTimer()
 };
+
+
+
+
+
 
 
 function loseGame() {
     introPage.style.display = 'block';
     loseText.style.display = "block";
     quizAll.style.display = "none";
-    resultText.style.display = "none";
-    console.log("You Ran Out Of Time!"); 
-    };
+    console.log("You Ran Out Of Time!");
+};
 
 
 
@@ -123,9 +230,9 @@ var quizQuestions = [{
     id: 0,
     q: "Commonly used data types do NOT include:",
     a: [{ text: "strings", isCorrect: false },
-        { text: "booleans", isCorrect: false },
-        { text: "alerts", isCorrect: true },
-        { text: "numbers", isCorrect: false }
+    { text: "booleans", isCorrect: false },
+    { text: "alerts", isCorrect: true },
+    { text: "numbers", isCorrect: false }
     ]
 
 },
@@ -133,9 +240,9 @@ var quizQuestions = [{
     id: 1,
     q: "The condition of an if/else statement is enclosed within ______.",
     a: [{ text: "quotes", isCorrect: false },
-        { text: "curly brackets", isCorrect: false },
-        { text: "parentheses", isCorrect: true },
-        { text: "square brackets", isCorrect: false }
+    { text: "curly brackets", isCorrect: false },
+    { text: "parentheses", isCorrect: true },
+    { text: "square brackets", isCorrect: false }
     ]
 
 },
@@ -143,9 +250,9 @@ var quizQuestions = [{
     id: 2,
     q: "Arrays in Javascript can be used to store ______.",
     a: [{ text: "numbers and strings", isCorrect: false },
-        { text: "other arrays", isCorrect: false },
-        { text: "booleans", isCorrect: false },
-        { text: "all of the above", isCorrect: true }
+    { text: "other arrays", isCorrect: false },
+    { text: "booleans", isCorrect: false },
+    { text: "all of the above", isCorrect: true }
     ]
 
 },
@@ -153,9 +260,9 @@ var quizQuestions = [{
     id: 3,
     q: "String values must be enclosed within ______ when being assigned to variables.",
     a: [{ text: "quotes", isCorrect: true },
-        { text: "curly brackets", isCorrect: false },
-        { text: "commas", isCorrect: false },
-        { text: "parenthesis", isCorrect: false }
+    { text: "curly brackets", isCorrect: false },
+    { text: "commas", isCorrect: false },
+    { text: "parenthesis", isCorrect: false }
     ]
 
 },
@@ -163,9 +270,9 @@ var quizQuestions = [{
     id: 4,
     q: "A very useful tool used during development and debugging for printing content to the debugger is:",
     a: [{ text: "Javascript", isCorrect: false },
-        { text: "terminal/bash", isCorrect: false },
-        { text: "for loops", isCorrect: false },
-        { text: "console.log", isCorrect: true }
+    { text: "terminal/bash", isCorrect: false },
+    { text: "for loops", isCorrect: false },
+    { text: "console.log", isCorrect: true }
     ]
 
 },
@@ -173,25 +280,25 @@ var quizQuestions = [{
     id: 5,
     q: "",
     a: [{ text: "", isCorrect: false },
-        { text: "", isCorrect: false },
-        { text: "", isCorrect: false },
-        { text: "", isCorrect: false }
+    { text: "", isCorrect: false },
+    { text: "", isCorrect: false },
+    { text: "", isCorrect: false }
     ]
 
 }
 ]
 
-function askQuestion(id){
-question.innerText = quizQuestions[id].q;
-btnA.innerText = quizQuestions[id].a[0].text
-btnB.innerText = quizQuestions[id].a[1].text
-btnC.innerText = quizQuestions[id].a[2].text
-btnD.innerText = quizQuestions[id].a[3].text
+function askQuestion(id) {
+    question.innerText = quizQuestions[id].q;
+    btnA.innerText = quizQuestions[id].a[0].text
+    btnB.innerText = quizQuestions[id].a[1].text
+    btnC.innerText = quizQuestions[id].a[2].text
+    btnD.innerText = quizQuestions[id].a[3].text
 
-btnA.value = quizQuestions[id].a[0].isCorrect;
-btnB.value = quizQuestions[id].a[1].isCorrect;
-btnC.value = quizQuestions[id].a[2].isCorrect;
-btnD.value = quizQuestions[id].a[3].isCorrect;
+    btnA.value = quizQuestions[id].a[0].isCorrect;
+    btnB.value = quizQuestions[id].a[1].isCorrect;
+    btnC.value = quizQuestions[id].a[2].isCorrect;
+    btnD.value = quizQuestions[id].a[3].isCorrect;
 }
 //The value the user chooses
 var myAnswer = ""
@@ -200,17 +307,17 @@ var myAnswer = ""
 
 // correct and wrong answer message fade
 
-function yesTextFade(){
+function yesTextFade() {
     yesText.style.display = "block"
     yesText.style.transition = 'none';
     yesText.style.opacity = '1';
     void yesText.offsetWidth;
     yesText.style.transition = 'opacity 1.5s';
     yesText.style.opacity = '0';
-   
+
 }
 
-function noTextFade(){
+function noTextFade() {
     noText.style.display = "block"
     noText.style.transition = 'none';
     noText.style.opacity = '1';
@@ -222,8 +329,8 @@ function noTextFade(){
 // event listeners for answers
 btnA.addEventListener("click", () => {
     myAnswer = btnA.value
-   
-   
+
+
     if (myAnswer == "true" && id < 5) {
         noText.style.display = "none"
         yesText.style.display = "none"
@@ -233,18 +340,18 @@ btnA.addEventListener("click", () => {
         console.log("true")
         yesTextFade()
     }
-    
+
     if (myAnswer == "false" && id < 5) {
         noText.style.display = "none"
         yesText.style.display = "none"
-        timerCount = timerCount -10
+        timerCount = timerCount - 10
         id++;
         askQuestion(id);
         console.log(id);
         console.log("false")
         noTextFade()
     }
-    if (id == 5){
+    if (id == 5) {
         console.log("end result");
         finishQuiz();
     }
@@ -260,18 +367,18 @@ btnB.addEventListener("click", () => {
         console.log("true")
         yesTextFade()
     }
-    
+
     if (myAnswer == "false" && id < 5) {
         noText.style.display = "none"
         yesText.style.display = "none"
-        timerCount = timerCount -10
+        timerCount = timerCount - 10
         id++;
         askQuestion(id);
         console.log(id);
         console.log("false")
         noTextFade()
     }
-    if (id == 5){
+    if (id == 5) {
         console.log("end result");
         finishQuiz();
     }
@@ -288,18 +395,18 @@ btnC.addEventListener("click", () => {
         console.log("true")
         yesTextFade()
     }
-    
+
     if (myAnswer == "false" && id < 5) {
         noText.style.display = "none"
         yesText.style.display = "none"
-        timerCount = timerCount -10
+        timerCount = timerCount - 10
         id++;
         askQuestion(id);
         console.log(id);
         console.log("false")
         noTextFade()
     }
-    if (id == 5){
+    if (id == 5) {
         console.log("end result");
         finishQuiz();
     }
@@ -317,18 +424,18 @@ btnD.addEventListener("click", () => {
         console.log("true")
         yesTextFade()
     }
-    
+
     if (myAnswer == "false" && id < 5) {
         noText.style.display = "none"
         yesText.style.display = "none"
-        timerCount = timerCount -10
+        timerCount = timerCount - 10
         id++;
         askQuestion(id);
         console.log(id);
         console.log("false")
         noTextFade()
     }
-    if (id == 5){
+    if (id == 5) {
         console.log("end result");
         finishQuiz();
     }
